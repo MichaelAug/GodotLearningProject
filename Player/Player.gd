66f01a0ebeafd5f97126ba24693af9aba2e4043d
Michael <michael.aug@protonmail.com>
2020-05-11 11:47:11 +0100

    Improved player movement

--- Player/Player.gd
+++ Player/Player.gd
@@ -1,15 +1,23 @@
 extends KinematicBody2D
 
+const MAX_SPEED = 200
+const ACCELERATION = 1
+const FRICTION = 50
+
 var velocity = Vector2.ZERO
 
 func _physics_process(delta):
 	var input_vector = Vector2.ZERO
 	input_vector.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
 	input_vector.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
+	input_vector = input_vector.normalized()
 	
 	if input_vector != Vector2.ZERO:
-		velocity = input_vector
+		velocity += input_vector * ACCELERATION
+		velocity = velocity.clamped(MAX_SPEED * delta)
 	else:
-		velocity = Vector2.ZERO
+		velocity = velocity.move_toward(Vector2.ZERO, FRICTION * delta)
 	
+	print(velocity)
+	#Multiply by delta so the velocity would be frame rate independent
 	move_and_collide(velocity)
